bc8a72fd079ddc439fbf148f75364ea450fc31fb
Matheus Iago dos Santos Silva <162377902+iago-developer@users.noreply.github.com>
2024-08-25 16:55:20 -0300

    Update transition of dates

--- server/db.js
+++ server/db.js
@@ -19,9 +19,6 @@
 
 const mongoose = require('mongoose');
 
-const DB_USER = 'Ana';
-const DB_PASSWORD = encodeURIComponent('123');
-
 mongoose.connect('mongodb://127.0.0.1:27017/Banco')
 
 .then(() => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,40 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Cooks = require('../models/Cooks');
-var Waiters = require('../models/Waiters');
+var Cooks = require("../models/Cooks");
+var Waiters = require("../models/Waiters");
 
 /* GET users listing. */
-router.get('/user/user', async function(req, res, next) {
+router.get("/user/user", async function (req, res, next) {
   const cooks = await Cooks.find();
   const waiters = await Waiters.find();
 
-  waiters.forEach(waiter => {
-    console.log(waiter.fullname)
+  waiters.forEach((waiter) => {
+    console.log(waiter.fullname);
   });
   res.render("user/user", { title: "Página do Usuário", cooks, waiters });
 });
 
-router.get('/user/redirect', function(req, res, next) {
-  res.render("user/redirect", { title: "Página do Usuário" });
+router.get("/user/table_office_hours", function (req, res, next) {
+  const horario_entrada = null;
+  const horario_saida = null;
+  res.render("user/table_office_hours", {
+    title: "Registros de Expediente",
+    horario_entrada,
+    horario_saida,
+  });
 });
 
-router.get('/user/table_office_hours', function(req, res, next) {
-  res.render("user/table_office_hours", { title: "Registros de Expediente" });
-});
-
-router.get('/user/new_office_hour', function(req, res, next) {
+router.get("/user/new_office_hour", function (req, res, next) {
   res.render("user/new_office_hour", { title: "Novo registro de Expediente" });
 });
 
-router.get('/user/wage', function(req, res, next) {
+router.get("/user/wage", function (req, res, next) {
   res.render("user/wage", { title: "Tabela de Salário" });
 });
 
-router.post('/user/redirect', (req, res, next) => {
+router.post("/user/table_office_hours", (req, res, next) => {
   const horario_entrada = req.body.horario_entrada;
   const horario_saida = req.body.horario_saida;
-  
-  res.render("user/table_office_hours", { horario_entrada, horario_saida });
+  const date = new Date();
+  const dia = String(date.getDate()).padStart(2, "0");
+  const mes = String(date.getMonth() + 1).padStart(2, "0");
+  const date_formated = `${dia}/${mes}`;
+  const data_formatada = date_formated;
+
+  res.render("user/table_office_hours", { horario_entrada, horario_saida, data_formatada });
 });
 
 module.exports = router;
